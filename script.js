df3ceed1bef55c725f61870ebd650057c89cbe4f
WALWALASAFI <walwalasafi99@gmail.com>
2024-06-26 16:25:12 +0430

    add space

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const textInput = document.getElementById('text-input');
-    const checkBtn = document.getElementById('check-btn');
-    const resultDiv = document.getElementById('result');
-  
-    checkBtn.addEventListener('click', function() {
-      const inputValue = textInput.value.trim();
-  
-      if (inputValue === '') {
-        alert('Please input a value');
-        return;
-      }
-  
-      const isPalindrome = checkPalindrome(inputValue);
-  
-      if (isPalindrome) {
-        resultDiv.textContent = `${inputValue} is a palindrome`;
-      } else {
-        resultDiv.textContent = `${inputValue} is not a palindrome`;
-      }
-    });
-  
-    function checkPalindrome(str) {
-      const alphanumericStr = str.replace(/[\W_]/g, '').toLowerCase();
-      const reversedStr = alphanumericStr.split('').reverse().join('');
-      return alphanumericStr === reversedStr;
+document.addEventListener('DOMContentLoaded', function () {
+  const textInput = document.getElementById('text-input');
+  const checkBtn = document.getElementById('check-btn');
+  const resultDiv = document.getElementById('result');
+
+  checkBtn.addEventListener('click', function () {
+    const inputValue = textInput.value.trim();
+
+    if (inputValue === '') {
+      alert('Please input a value');
+      return;
+    }
+
+    const isPalindrome = checkPalindrome(inputValue);
+
+    if (isPalindrome) {
+      resultDiv.textContent = `${inputValue} is a palindrome`;
+    } else {
+      resultDiv.textContent = `${inputValue} is not a palindrome`;
     }
   });
-  
+
+  function checkPalindrome(str) {
+    const alphanumericStr = str.replace(/[\W_]/g, '').toLowerCase();
+    const reversedStr = alphanumericStr.split('').reverse().join('');
+    return alphanumericStr === reversedStr;
+  }
+});
